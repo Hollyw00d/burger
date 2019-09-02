f34bd1a6902b37a583be7ba40b1d9b2ad2945ef2
Matt Jennings <matt@mattjennings.net>
2019-09-02 16:39:09 -0700

    added front end POST call

--- public/assets/js/global.js
+++ public/assets/js/global.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-    
+    // Devour burger
+    $('#devour-burger').on('click', function(e) {
+        e.preventDefault();
+        var firstBurgerNotDevouredId = +($('li').first().attr('data-id'));
+        var firstBurgerNotDevouredIdObj = {
+            firstBurgerNotDevouredId
+        };
+
+        $.post('/api/burgerdevoured', firstBurgerNotDevouredIdObj)
+            .done(function(response) {
+                console.log(response);
+            })
+            .fail(function(xhr, status, error) {
+                console.log(`Error: ${xhr.responseText}`);
+            });
+
+    });
 });
